99f4bc0faa020a22f9b5d5902d4a954e2e8ddbf8
hughescard <guillehughes2004@gmail.com>
2025-12-29 12:33:59 -0500

    feat: add a grave for lost pieces

--- src/UI/Renderer.hs
+++ src/UI/Renderer.hs
@@ -5,7 +5,7 @@ where
 
 import Board.Core (Board, cellPiece, cellPosition)
 import Data.Maybe (fromMaybe)
-import GameState (gsBoard)
+import GameState (gsBoard, gsCapturedByBlack, gsCapturedByWhite)
 import Graphics.Gloss
   ( Picture,
     circleSolid,
@@ -39,6 +39,7 @@ drawGame state =
         [ drawBoard sqSize,
           drawHighlights sqSize state,
           drawPieces sqSize (uiAssets state) (gsBoard gameState),
+          drawGraveyards state,
           drawMessage state
         ]
 
@@ -128,6 +129,119 @@ drawPiece sqSize assets pos piece =
       (sprite, spriteSize) = pieceSprite assets piece
       scaleFactor = (sqSize * 0.85) / spriteSize
    in translate x y $ scale scaleFactor scaleFactor sprite
+
+data GraveyardStyle = GraveyardStyle
+  { gyLabel :: String,
+    gyPanelColor :: Gloss.Color,
+    gyPanelBorder :: Gloss.Color,
+    gySignColor :: Gloss.Color,
+    gySignTextColor :: Gloss.Color,
+    gySlotColor :: Gloss.Color
+  }
+
+drawGraveyards :: UIState -> Picture
+drawGraveyards state =
+  let sqSize = uiSquareSize state
+      (winW, winH) = uiWindowSize state
+      boardSize = sqSize * 8
+      boardHalf = boardSize / 2
+      sideWidth = max 0 ((winW - boardSize) / 2)
+      iconSize = max 18 (sqSize * 0.4)
+      padding = iconSize * 0.2
+      panelWidth = sideWidth
+      panelHeight = winH
+      panelY = 0
+      signHeight = iconSize * 0.7
+      signY = panelY + panelHeight / 2 - signHeight / 2 - 6
+      startY = signY - signHeight / 2 - padding - iconSize / 2
+      leftX = -boardHalf - panelWidth / 2
+      rightX = boardHalf + panelWidth / 2
+      gameState = uiGameState state
+      blackLost = gsCapturedByWhite gameState
+      whiteLost = gsCapturedByBlack gameState
+      whiteStyle =
+        GraveyardStyle
+          { gyLabel = "Cementerio Blancas",
+            gyPanelColor = makeColorI 50 50 55 220,
+            gyPanelBorder = makeColorI 20 20 25 230,
+            gySignColor = makeColorI 230 230 235 255,
+            gySignTextColor = makeColorI 30 30 35 255,
+            gySlotColor = makeColorI 80 80 90 200
+          }
+      blackStyle =
+        GraveyardStyle
+          { gyLabel = "Cementerio Negras",
+            gyPanelColor = makeColorI 235 230 220 230,
+            gyPanelBorder = makeColorI 165 155 145 240,
+            gySignColor = makeColorI 40 40 45 255,
+            gySignTextColor = makeColorI 235 235 235 255,
+            gySlotColor = makeColorI 210 200 190 220
+          }
+   in if panelWidth <= 0
+        then pictures []
+        else
+          pictures
+            [ drawGraveyardColumn (uiAssets state) whiteStyle whiteLost leftX panelY panelWidth panelHeight signY signHeight startY iconSize padding,
+              drawGraveyardColumn (uiAssets state) blackStyle blackLost rightX panelY panelWidth panelHeight signY signHeight startY iconSize padding
+            ]
+
+drawGraveyardColumn ::
+  Assets ->
+  GraveyardStyle ->
+  [Piece] ->
+  Float ->
+  Float ->
+  Float ->
+  Float ->
+  Float ->
+  Float ->
+  Float ->
+  Float ->
+  Float ->
+  Picture
+drawGraveyardColumn assets style pieces x panelY panelWidth panelHeight signY signHeight startY iconSize padding =
+  let panelPic = drawGraveyardPanel style x panelY panelWidth panelHeight
+      signPic = drawGraveyardSign style x signY panelWidth signHeight
+      piecePics =
+        [ drawMiniPiece assets (gySlotColor style) iconSize x (startY - fromIntegral idx * (iconSize + padding)) piece
+          | (idx, piece) <- zip [0 :: Int ..] pieces
+        ]
+   in pictures (panelPic : signPic : piecePics)
+
+drawGraveyardPanel :: GraveyardStyle -> Float -> Float -> Float -> Float -> Picture
+drawGraveyardPanel style x y w h =
+  let borderSize = 3
+   in pictures
+        [ translate x y $ color (gyPanelBorder style) $ rectangleSolid (w + borderSize) (h + borderSize),
+          translate x y $ color (gyPanelColor style) $ rectangleSolid w h
+        ]
+
+drawGraveyardSign :: GraveyardStyle -> Float -> Float -> Float -> Float -> Picture
+drawGraveyardSign style x y w h =
+  let baseScale = 0.12
+      label = gyLabel style
+      maxWidth = max 10 (w - 20)
+      rawWidth = fromIntegral (length label) * 100 * baseScale
+      textScale = min baseScale (maxWidth / rawWidth)
+      labelWidth = fromIntegral (length label) * 100 * textScale
+      labelX = x - labelWidth / 2
+      labelY = y - (h * 0.3)
+   in pictures
+        [ translate x y $ color (gySignColor style) $ rectangleSolid (w - 12) h,
+          translate labelX labelY $
+            scale textScale textScale $
+              color (gySignTextColor style) $
+                text label
+        ]
+
+drawMiniPiece :: Assets -> Gloss.Color -> Float -> Float -> Float -> Piece -> Picture
+drawMiniPiece assets slotColor iconSize x y piece =
+  let (sprite, spriteSize) = pieceSprite assets piece
+      scaleFactor = iconSize / spriteSize
+   in pictures
+        [ translate x y $ color slotColor $ rectangleSolid (iconSize * 1.15) (iconSize * 1.15),
+          translate x y $ scale scaleFactor scaleFactor sprite
+        ]
 
 drawMessage :: UIState -> Picture
 drawMessage state =
